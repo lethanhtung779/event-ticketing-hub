46b75733f662b0e648234aeee7cd776b2288e7d9
lethanhtung779 <lethanhtungylt1@gmail.com>
2026-07-14 22:42:07 +0700

    fix: force Node.js DNS to prefer IPv4

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import helmet from 'helmet';
+import { setDefaultResultOrder } from 'dns';
+setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
